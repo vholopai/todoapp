6127d9e8e3fa44f06c0906b3d77e9ade0c2a0d11
Visa Holopainen <visa.holopainen@lago.lan>
2020-08-16 19:43:24 +0300

    Minor refactoring

--- static/main.js
+++ static/main.js
@@ -15,6 +15,38 @@
 	    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	    xhttp.send(data);
 	}
+	
+	function showTodoItem(todoDiv, obj, key) {
+		var temp = document.getElementsByTagName("template")[0]; // todo template
+		var clone = temp.content.cloneNode(true);
+		todoDiv.appendChild(clone);
+		var cloneDiv = document.getElementById("0");
+		// todo id goes here:
+		cloneDiv.id = obj[key].id;
+		cloneDiv.style.display = "block";
+		cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
+		// todo text content goes here:
+		cloneDiv.getElementsByClassName('card-body')[0]
+			.getElementsByClassName('card-text')[0]
+				.innerHTML =
+					obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');	
+	}	
+	
+	function showDoneItem(doneDiv, obj, key) {
+		var temp = document.getElementsByTagName("template")[1]; // done template
+		var clone = temp.content.cloneNode(true);
+		doneDiv.appendChild(clone);
+		var cloneDiv = document.getElementById("00");
+		// done id goes here:
+		cloneDiv.id = obj[key].id;
+		cloneDiv.style.display = "block";
+		cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
+		// done text content goes here:
+		cloneDiv.getElementsByClassName('card-body')[0]
+			.getElementsByClassName('card-text')[0]
+				.innerHTML =
+					obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');		
+	}
 		
 	// render the (todo and done) items we got from the server as JSON response
 	function getAllTodosCallback(data) {
@@ -33,34 +65,10 @@
 			if (obj.hasOwnProperty(key)) {
 				var type = obj[key].type;
 				if (type === "todo") {
-					var temp = document.getElementsByTagName("template")[0]; // todo template
-					var clone = temp.content.cloneNode(true);
-					todoDiv.appendChild(clone);
-					var cloneDiv = document.getElementById("0");
-					// todo id goes here:
-					cloneDiv.id = obj[key].id;
-					cloneDiv.style.display = "block";
-					cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
-					// todo text content goes here:
-					cloneDiv.getElementsByClassName('card-body')[0]
-						.getElementsByClassName('card-text')[0]
-							.innerHTML =
-								obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');
+					showTodoItem(todoDiv, obj, key);
 				}
 				else { // done item
-					var temp = document.getElementsByTagName("template")[1]; // done template
-					var clone = temp.content.cloneNode(true);
-					doneDiv.appendChild(clone);
-					var cloneDiv = document.getElementById("00");
-					// done id goes here:
-					cloneDiv.id = obj[key].id;
-					cloneDiv.style.display = "block";
-					cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
-					// done text content goes here:
-					cloneDiv.getElementsByClassName('card-body')[0]
-						.getElementsByClassName('card-text')[0]
-							.innerHTML =
-								obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');						
+					showDoneItem(doneDiv, obj, key);
 				}
 			}
 		}	
