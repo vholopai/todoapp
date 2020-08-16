437066a6a60e2cc2fb753913c6c9f608cedcfa8e
Visa Holopainen <visa.holopainen@lago.lan>
2020-08-16 19:12:53 +0300

    Minor indentation change

--- static/main.js
+++ static/main.js
@@ -1,102 +1,102 @@
 
-		// send a HTTP POST request to the server
-		function post(okFunction, failFunction, uri, data) {
-		    var xhttp = new XMLHttpRequest();
-		    xhttp.onreadystatechange = function() {
-		        if (this.readyState > 3 && this.status == 200) {
-		        	okFunction(this);
-		        }
-		        else if (this.readyState > 3 && this.status > 200) {
-		            failFunction(this.status);
-		        }			        
-		    };
-		    xhttp.open("POST", uri); 
-		    //xhttp.withCredentials = true;
-		    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		    xhttp.send(data);
-		}
-			
-		// render the (todo and done) items we got from the server as JSON response
-		function getAllTodosCallback(data) {
-			var obj;
-			try {
-				obj = JSON.parse(data.responseText);
-			} catch (err) {
-				console.log("Unable to parse JSON " + data.responseText);
-				return;
-			}
-			var todoDiv = document.getElementById('todoItems');
-			todoDiv.innerHTML = "";
-			var doneDiv = document.getElementById('doneItems');
-			doneDiv.innerHTML = "";
-			for (var key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					var type = obj[key].type;
-					if (type === "todo") {
-						var temp = document.getElementsByTagName("template")[0]; // todo template
-						var clone = temp.content.cloneNode(true);
-						todoDiv.appendChild(clone);
-						var cloneDiv = document.getElementById("0");
-						// todo id goes here:
-						cloneDiv.id = obj[key].id;
-						cloneDiv.style.display = "block";
-						cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
-						// todo text content goes here:
-						cloneDiv.getElementsByClassName('card-body')[0]
-							.getElementsByClassName('card-text')[0]
-								.innerHTML =
-									obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');
-					}
-					else { // done item
-						var temp = document.getElementsByTagName("template")[1]; // done template
-						var clone = temp.content.cloneNode(true);
-						doneDiv.appendChild(clone);
-						var cloneDiv = document.getElementById("00");
-						// done id goes here:
-						cloneDiv.id = obj[key].id;
-						cloneDiv.style.display = "block";
-						cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
-						// done text content goes here:
-						cloneDiv.getElementsByClassName('card-body')[0]
-							.getElementsByClassName('card-text')[0]
-								.innerHTML =
-									obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');						
-					}
-				}
-			}	
-		}
-
-		function setAsDone(elem) {
-			post(getAllTodosCallback, failCallback, "/setAsDone", "text=" + elem.parentNode.parentNode.id);
-		}
-			
-		function setAsTodo(elem) {
-			post(getAllTodosCallback, failCallback, "/setAsTodo", "text=" + elem.parentNode.parentNode.id);
-		}			
-
-		function removeTodo(elem) {
-			post(getAllTodosCallback, failCallback, "/removeTodo", "text=" + elem.parentNode.parentNode.id);
-		}			
-			
-		function addTodo() {
-			var elem = document.getElementById("newTodoText");
-			var content = elem.value;
-			elem.value = "";
-			var deadlineElem = document.getElementById("timepicker1")
-			var deadline = deadlineElem.value;
-			if (deadline !== "") {
-				content += "\n\n" + "Deadline: " + deadline;
-				deadlineElem.value = "";
-			}
-			post(getAllTodosCallback, failCallback, "/addTodo",
-					"text=" + encodeURIComponent(content));
-		}
+	// send a HTTP POST request to the server
+	function post(okFunction, failFunction, uri, data) {
+	    var xhttp = new XMLHttpRequest();
+	    xhttp.onreadystatechange = function() {
+	        if (this.readyState > 3 && this.status == 200) {
+	        	okFunction(this);
+	        }
+	        else if (this.readyState > 3 && this.status > 200) {
+	            failFunction(this.status);
+	        }			        
+	    };
+	    xhttp.open("POST", uri); 
+	    //xhttp.withCredentials = true;
+	    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	    xhttp.send(data);
+	}
 		
-		function failCallback(code) {
-			alert("Failed with code " + code);
+	// render the (todo and done) items we got from the server as JSON response
+	function getAllTodosCallback(data) {
+		var obj;
+		try {
+			obj = JSON.parse(data.responseText);
+		} catch (err) {
+			console.log("Unable to parse JSON " + data.responseText);
+			return;
 		}
-			
-		// after page load, get all
-		(function() {
-			post(getAllTodosCallback, failCallback, "/getAllTodos", "");
-		})();
+		var todoDiv = document.getElementById('todoItems');
+		todoDiv.innerHTML = "";
+		var doneDiv = document.getElementById('doneItems');
+		doneDiv.innerHTML = "";
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				var type = obj[key].type;
+				if (type === "todo") {
+					var temp = document.getElementsByTagName("template")[0]; // todo template
+					var clone = temp.content.cloneNode(true);
+					todoDiv.appendChild(clone);
+					var cloneDiv = document.getElementById("0");
+					// todo id goes here:
+					cloneDiv.id = obj[key].id;
+					cloneDiv.style.display = "block";
+					cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
+					// todo text content goes here:
+					cloneDiv.getElementsByClassName('card-body')[0]
+						.getElementsByClassName('card-text')[0]
+							.innerHTML =
+								obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');
+				}
+				else { // done item
+					var temp = document.getElementsByTagName("template")[1]; // done template
+					var clone = temp.content.cloneNode(true);
+					doneDiv.appendChild(clone);
+					var cloneDiv = document.getElementById("00");
+					// done id goes here:
+					cloneDiv.id = obj[key].id;
+					cloneDiv.style.display = "block";
+					cloneDiv.getElementsByClassName('card-title')[0].innerHTML = obj[key].id;
+					// done text content goes here:
+					cloneDiv.getElementsByClassName('card-body')[0]
+						.getElementsByClassName('card-text')[0]
+							.innerHTML =
+								obj[key].msg.replace(/(?:\r\n|\r|\n)/g, '<br>');						
+				}
+			}
+		}	
+	}
+
+	function setAsDone(elem) {
+		post(getAllTodosCallback, failCallback, "/setAsDone", "text=" + elem.parentNode.parentNode.id);
+	}
+		
+	function setAsTodo(elem) {
+		post(getAllTodosCallback, failCallback, "/setAsTodo", "text=" + elem.parentNode.parentNode.id);
+	}			
+
+	function removeTodo(elem) {
+		post(getAllTodosCallback, failCallback, "/removeTodo", "text=" + elem.parentNode.parentNode.id);
+	}			
+		
+	function addTodo() {
+		var elem = document.getElementById("newTodoText");
+		var content = elem.value;
+		elem.value = "";
+		var deadlineElem = document.getElementById("timepicker1")
+		var deadline = deadlineElem.value;
+		if (deadline !== "") {
+			content += "\n\n" + "Deadline: " + deadline;
+			deadlineElem.value = "";
+		}
+		post(getAllTodosCallback, failCallback, "/addTodo",
+				"text=" + encodeURIComponent(content));
+	}
+	
+	function failCallback(code) {
+		alert("Failed with code " + code);
+	}
+		
+	// after page load, get all
+	(function() {
+		post(getAllTodosCallback, failCallback, "/getAllTodos", "");
+	})();
