9dc6a0233b3d7ea7c7df0657e1bc2d6e95f7c2a7
Visa Holopainen <visa.holopainen@lago.lan>
2020-08-18 18:46:08 +0300

    Minor refactoring

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -4,14 +4,12 @@ import java.io.File;
 
 public class Constants {
     // never modified
-    public static final String VIEWPATH = "./" + File.separator + "static" + File.separator;
+    public static final String VIEWPATH = "." + File.separator + "static" + File.separator;
+    public static final String COUNTER_FILE = "counter.txt";
+    public static final String ITEM_FILE_EXT = ".todo";
     
     // modified from junit tests, hence not final
     public static String TODOPATH = "." + File.separator + "data" + File.separator + "todo" + File.separator;
     public static String DONEPATH = "." + File.separator + "data" + File.separator + "done" + File.separator;
     public static String REMOVEDPATH = "." + File.separator + "data" + File.separator + "removed" + File.separator; 
-    public static final String COUNTER_FILE = "counter.txt";
-    public static final String ITEM_FILE_EXT = ".todo";
-
-
 }
